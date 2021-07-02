da2f8e9456e1e40c424f3b606c12412e44a7a895
Mark <mark@example.dev>
2021-07-02 00:01:33 -0700

    created helpers and payments test files

---
+++ tip-pool/payments.test.js
@@ -0,0 +1,13 @@
+describe('Payments tests (with set-up and tear-down', function(){
+    beforeEach(function(){
+        billAmtInput.value = 30;
+        tipAmtInput.value = 4.2;
+    })
+
+    it('should ')
+
+    afterEach(function(){
+        allPayments = {};
+        paymentId = 0;
+    })
+})
