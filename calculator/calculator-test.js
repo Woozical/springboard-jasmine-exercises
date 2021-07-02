441fffb03f349655d0c33ac2e729e63054667dd9
Mark <mark@example.dev>
2021-07-01 22:11:13 -0700

    Implemetation and unit testing

--- calculator/calculator-test.js
+++ calculator/calculator-test.js
@@ -1,11 +1,29 @@
 
 it('should calculate the monthly rate correctly', function () {
-  // ...
+  expect(calculateMonthlyPayment({amount:200000,years:15,rate:0.06})).toEqual('$1687.71');
+  expect(calculateMonthlyPayment({amount:5000,years:2,rate:0.05})).toEqual('$219.36');
+  expect(calculateMonthlyPayment({amount:1200,years:1,rate:0})).toEqual('$100.00');
 });
 
 
 it("should return a result with 2 decimal places", function() {
-  // ..
+
+  function getDecString(values){
+    let result = calculateMonthlyPayment(values);
+    return result.slice(result.indexOf('.')+1);
+  }
+
+  expect(getDecString({amount:20000,years:15,rate:0.30}).length).toEqual(2);
+  expect(getDecString({amount:200.00,years:1.5,rate:30}).length).toEqual(2);
+  expect(getDecString({amount:209253246,years:102315,rate:3.00023523500000}).length).toEqual(2);
+});
+
+it("should handle fringe and invalid inputs", function(){
+  let errStr = 'Invalid input.\nTry again.';
+  expect(calculateMonthlyPayment({amount:300,years:-12,rate:342})).toEqual(errStr);
+  expect(calculateMonthlyPayment({})).toEqual(errStr);
+  expect(calculateMonthlyPayment({amount:0, years:0, rate:0})).toEqual(errStr);
+  expect(calculateMonthlyPayment({amount:'300',years:true,rate:0.3})).toEqual(errStr);
 });
 
 /// etc
--- calculator/calculator.js
+++ calculator/calculator.js
@@ -21,20 +21,52 @@ function getCurrentUIValues() {
 // Put some default values in the inputs
 // Call a function to calculate the current monthly payment
 function setupIntialValues() {
+  const amountInput = document.querySelector('#loan-amount');
+  const yearsInput = document.querySelector('#loan-years');
+  const rateInput = document.querySelector('#loan-rate');
+
+  amountInput.value = 5000;
+  yearsInput.value = 2;
+  rateInput.value = .05;
+
+  update();
 }
 
 // Get the current values from the UI
 // Update the monthly payment
 function update() {
+  // let currentVal = getCurrentUIValues();
+  // let monthlyStr = calculateMonthlyPayment(currentVal);
+  // updateMonthly(monthlyStr);
+  updateMonthly(calculateMonthlyPayment(getCurrentUIValues()));
 }
 
 // Given an object of values (a value has amount, years and rate ),
 // calculate the monthly payment.  The output should be a string
 // that always has 2 decimal places.
 function calculateMonthlyPayment(values) {
+  const interest = values.rate / 12;
+  const payments = values.years * 12;
+  let mPay;
+
+  // Check for invalid object values on parameter 'values'
+  if(!values.amount || (!values.rate && values.rate !== 0) || !values.years ||
+  values.years < 0 || values.amount < 0 || values.rate < 0 ||
+  values.amount === true || values.years === true || values.rate === true){
+    return 'Invalid input.\nTry again.'
+  }  
+
+  // Prevent dividing by 0
+  if (interest === 0){
+    mPay = values.amount / payments;
+  } else {
+    mPay = (values.amount * (interest)) / (1 - ((1 + interest)**(payments * -1)));
+  }
+  return '$' + mPay.toFixed(2);
 }
 
 // Given a string representing the monthly payment value,
 // update the UI to show the value.
 function updateMonthly(monthly) {
+  document.querySelector('#monthly-payment').innerText = monthly;
 }
